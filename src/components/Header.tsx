e6da6fa3797496a5acbcda75adc7ace3b83380a3
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-03-10 14:22:19 +0330

    feat: user card and delete all products

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,44 @@
 import { FaShoppingBasket } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import styles from "./Header.module.css";
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
+import { MdDelete } from "react-icons/md";
 
 function Header() {
-  const cart = useContext(CartContext);
+  const context = useContext(CartContext);
+
+  const location = useLocation();
+
+  const clickHandler = () => {
+    swal({
+      title: "Are you sure?",
+      icon: "warning",
+      buttons: ["No", "Yes"],
+    }).then((res) => {
+      if (res) {
+        context.removeAll();
+      }
+    });
+  };
 
   return (
     <header className={styles.header}>
       <Link to="/">Shopping Store</Link>
-      <Link to="/cart" className={styles.cart}>
-        <FaShoppingBasket />
-        {cart.userCart.length > 0 ? <span>{cart.userCart.length}</span> : null}
-      </Link>
+      {location.pathname === "/" ? (
+        <Link to="/cart" className={styles.cart}>
+          <FaShoppingBasket />
+          {context.userCart.length > 0 ? (
+            <span>{context.userCart.length}</span>
+          ) : null}
+        </Link>
+      ) : (
+        <button className={styles.button} onClick={clickHandler}>
+          <MdDelete />
+          Remove All Products
+        </button>
+      )}
     </header>
   );
 }
--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useContext } from "react";
+import styles from "./Cart.module.css";
+import { CartContext } from "../../context/CartContext";
+import Card from "../../components/Card";
 
 function Cart() {
+  const context = useContext(CartContext);
+
   return (
-    <div>Cart</div>
-  )
+    <div className={styles.cart}>
+      {context.userCart.length ? context.userCart.map((product) => (
+        <Card key={product.id} {...product} />
+      )) : <p className={styles.empty}>Empty...</p>}
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
