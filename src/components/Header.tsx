04b7dc980320c280667a1cf6de9d98c65194ea6d
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-03-10 13:23:20 +0330

    feat: userCart count in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,19 @@
 import { FaShoppingBasket } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
 
 function Header() {
+  const cart = useContext(CartContext);
+
   return (
     <header className={styles.header}>
       <Link to="/">Shopping Store</Link>
       <Link to="/cart" className={styles.cart}>
         <FaShoppingBasket />
-        <span>0</span>
+        {cart.userCart.length > 0 ? <span>{cart.userCart.length}</span> : null}
       </Link>
     </header>
   );
