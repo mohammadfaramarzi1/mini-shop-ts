6dbf62cabdd69541f723dcaff14d535a26c790eb
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-03-10 14:10:55 +0330

    feat: addProduct to cart

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,8 +2,29 @@ import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 import { Product } from "./Products.types";
 
 import styles from "./Card.module.css";
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+import swal from "sweetalert";
+import { useNavigate } from "react-router-dom";
 
-function Card({ image, title, price, rating: { rate } }: Product) {
+function Card({ image, title, price, rating: { rate }, id }: Product) {
+  const context = useContext(CartContext);
+  const navigate = useNavigate()
+
+  const clickHandler = () => {
+    context.addProduct(id);
+    swal({
+      title: "Added to cart successfully",
+      icon: "success",
+      buttons: ["ok", "go to cart"],
+    })
+    .then(res => {
+        if(res) {
+            navigate("/cart")
+        }
+    })
+  };
+
   return (
     <div className={styles.card}>
       <img src={image} alt="" />
@@ -15,12 +36,12 @@ function Card({ image, title, price, rating: { rate } }: Product) {
               <AiFillStar style={{ color: "orange" }} />
             ))}
             {Array.from({ length: 5 - Math.floor(rate) }).map((_, index) => (
-              <AiOutlineStar style={{color: "orange"}} />
+              <AiOutlineStar style={{ color: "orange" }} />
             ))}
           </div>
           <p>${price}</p>
         </div>
-        <button>Add to Basket</button>
+        <button onClick={clickHandler}>Add to Basket</button>
       </main>
     </div>
   );
